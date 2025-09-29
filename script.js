921b87cbc38f97734dfcb4b20620d9e9e9ad3ccd
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-29 18:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopBtn = document.querySelector('.stop');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    stopSounds();
+    const soundId = btn.innerText;
+    const audioEl = document.getElementById(soundId);
+    if (audioEl) {
+      audioEl.play();
+    }
+  });
+});
+
+stopBtn.addEventListener('click', stopSounds);
+
+function stopSounds() {
+  const audios = document.querySelectorAll('audio');
+  audios.forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
